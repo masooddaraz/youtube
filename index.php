719c78504ab428d31e7c8e90725e1f9f0ba72b7d
masooddaraz <46477765+masooddaraz@users.noreply.github.com>
2020-07-23 16:23:28 +0500

    sd

--- index.php
+++ index.php
@@ -1,2 +1,1 @@
-<iframe width="100%" height="400" src="https://www.youtube.com/embed/tgbNymZ7vqY?autoplay=1" allow="autoplay; fullscreen">
-</iframe>
+<iframe width="100%" height="400" src="https://www.youtube.com/embed/tgbNymZ7vqY?autoplay=1" allow="autoplay; fullscreen"></iframe>
